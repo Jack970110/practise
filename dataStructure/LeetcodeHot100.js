df1b7b8bc80f095516ffe86670b0c6271f14f290
Jack0110 <wangjianping@sjtu.edu.cn>
2021-11-09 09:21:23 +0800

    2021.11.08

--- dataStructure/LeetcodeHot100.js
+++ dataStructure/LeetcodeHot100.js
@@ -2,41 +2,41 @@
 
 注意：答案中不可以包含重复的三元组 */
 /* 思路：三数之和为0，必有正有负，数组可以先排序。枚举三位数，第一位必为负，后两位相互关联可以用双指针，i+1,nums.length-1;sum<0，left++；sum>0,right--;同时要考虑去重 */
-var threeSum = function(nums) {
+var threeSum = function (nums) {
     let ans = [];
     const len = nums.length;
-    if(nums == null || len < 3) return ans;
+    if (nums == null || len < 3) return ans;
     nums.sort((a, b) => a - b); // 排序
-    for (let i = 0; i < len ; i++) {
-        if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
-        if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
-        let L = i+1;
-        let R = len-1;
-        while(L < R){
+    for (let i = 0; i < len; i++) {
+        if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+        if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+        let L = i + 1;
+        let R = len - 1;
+        while (L < R) {
             const sum = nums[i] + nums[L] + nums[R];
-            if(sum == 0){
-                ans.push([nums[i],nums[L],nums[R]]);
-                while (L<R && nums[L] == nums[L+1]) L++; // 去重
-                while (L<R && nums[R] == nums[R-1]) R--; // 去重
+            if (sum == 0) {
+                ans.push([nums[i], nums[L], nums[R]]);
+                while (L < R && nums[L] == nums[L + 1]) L++; // 去重
+                while (L < R && nums[R] == nums[R - 1]) R--; // 去重
                 L++;
                 R--;
             }
             else if (sum < 0) L++;
             else if (sum > 0) R--;
         }
-    }        
+    }
     return ans;
 };
 /* 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。 */
 /* 穷举可以，双循环复杂度较高；双指针去一重循环 */
-var maxArea = function(height) {
+var maxArea = function (height) {
     let water = 0, left = 0, right = height.length;
-    while(left < right){
+    while (left < right) {
         let min = Math.min(height[left], height[right]);
         water = min * (right - left) > water ? min * (right - left) : water;
-        if( height[left] < height[right]){
-            left ++;//左小右移
-        }else{
+        if (height[left] < height[right]) {
+            left++;//左小右移
+        } else {
             right--;//右小左移
         }
     }
@@ -61,134 +61,215 @@ var maxArea = function(height) {
  */
 /* 思路：parseInt()的要求大致相同，但是还需要满足额外；自动机；正则 */
 //parseInt()思路
-var myAtoi = function(s) {
+var myAtoi = function (s) {
     const number = parseInt(s, 10);
 
-    if(isNaN(number)) {
+    if (isNaN(number)) {
         return 0;
     } else if (number < Math.pow(-2, 31) || number > Math.pow(2, 31) - 1) {
         return number < Math.pow(-2, 31) ? Math.pow(-2, 31) : Math.pow(2, 31) - 1;
     } else {
         return number;
     }
-  }
+}
 //正则
-var myAtoi = function(s) {
+var myAtoi = function (s) {
     const re = new RegExp(/^(-|\+)?\d+/);
     let str = s.trim().match(re);
     let res = str ? Number(str[0]) : 0;
-    return res >= 0 ? Math.min(res, 2**31 - 1) : Math.max(res, -(2**31))
+    return res >= 0 ? Math.min(res, 2 ** 31 - 1) : Math.max(res, -(2 ** 31))
 };
 //自动机
 /**
  * @param {string} str
  * @return {number}
  */
- var myAtoi = function(str) {
+var myAtoi = function (str) {
     // 自动机类
-    class Automaton{
-      constructor() {
-        // 执行阶段，默认处于开始执行阶段
-        this.state = 'start';
-        // 正负符号，默认是正数
-        this.sign = 1;
-        // 数值，默认是0
-        this.answer = 0;
+    class Automaton {
+        constructor() {
+            // 执行阶段，默认处于开始执行阶段
+            this.state = 'start';
+            // 正负符号，默认是正数
+            this.sign = 1;
+            // 数值，默认是0
+            this.answer = 0;
+            /*
+            关键点：
+            状态和执行阶段的对应表
+            含义如下：
+            [执行阶段, [空格, 正负, 数值, 其他]]
+            */
+            this.map = new Map([
+                ['start', ['start', 'signed', 'in_number', 'end']],
+                ['signed', ['end', 'end', 'in_number', 'end']],
+                ['in_number', ['end', 'end', 'in_number', 'end']],
+                ['end', ['end', 'end', 'end', 'end']]
+            ])
+        }
+
+        // 获取状态的索引
+        getIndex(char) {
+            if (char === ' ') {
+                // 空格判断
+                return 0;
+            } else if (char === '-' || char === '+') {
+                // 正负判断
+                return 1;
+            } else if (typeof Number(char) === 'number' && !isNaN(char)) {
+                // 数值判断
+                return 2;
+            } else {
+                // 其他情况
+                return 3;
+            }
+        }
+
         /*
         关键点：
-        状态和执行阶段的对应表
-        含义如下：
-        [执行阶段, [空格, 正负, 数值, 其他]]
+        字符转换执行函数
         */
-        this.map = new Map([
-          ['start', ['start', 'signed', 'in_number', 'end']],
-          ['signed', ['end', 'end', 'in_number', 'end']],
-          ['in_number', ['end', 'end', 'in_number', 'end']],
-          ['end', ['end', 'end', 'end', 'end']]
-        ])
-      }
-  
-      // 获取状态的索引
-      getIndex(char) {
-        if (char === ' ') {
-          // 空格判断
-          return 0;
-        } else if (char === '-' || char === '+') {
-          // 正负判断
-          return 1;
-        } else if (typeof Number(char) === 'number' && !isNaN(char)) {
-          // 数值判断
-          return 2;
-        } else {
-          // 其他情况
-          return 3;
+        get(char) {
+            /*
+            易错点：
+            每次传入字符时，都要变更自动机的执行阶段
+            */
+            this.state = this.map.get(this.state)[this.getIndex(char)];
+
+            if (this.state === 'in_number') {
+                /*
+                小技巧：
+                在JS中，对字符串类型进行减法操作，可以将得到一个数值型（Number）的值
+        
+                易错点：
+                本处需要利用括号来提高四则运算的优先级
+                */
+                this.answer = this.answer * 10 + (char - 0);
+
+                /*
+                易错点：
+                在进行负数比较时，需要将INT_MIN变为正数
+                */
+                this.answer = this.sign === 1 ? Math.min(this.answer, Math.pow(2, 31) - 1) : Math.min(this.answer, -Math.pow(-2, 31));
+            } else if (this.state === 'signed') {
+                /*
+                优化点：
+                对于一个整数来说，非正即负，
+                所以正负号的判断，只需要一次。
+                故，可以降低其判断的优先级
+                */
+                this.sign = char === '+' ? 1 : -1;
+            }
         }
-      }
-  
-      /*
-      关键点：
-      字符转换执行函数
-      */
-      get(char) {
-        /*
-        易错点：
-        每次传入字符时，都要变更自动机的执行阶段
-        */
-        this.state = this.map.get(this.state)[this.getIndex(char)];
-  
-        if(this.state === 'in_number') {
-          /*
-          小技巧：
-          在JS中，对字符串类型进行减法操作，可以将得到一个数值型（Number）的值
-  
-          易错点：
-          本处需要利用括号来提高四则运算的优先级
-          */
-          this.answer = this.answer * 10 + (char - 0);
-  
-          /*
-          易错点：
-          在进行负数比较时，需要将INT_MIN变为正数
-          */
-          this.answer = this.sign === 1 ? Math.min(this.answer, Math.pow(2, 31) - 1) : Math.min(this.answer, -Math.pow(-2, 31));
-        } else if (this.state === 'signed') {
-          /*
-          优化点：
-          对于一个整数来说，非正即负，
-          所以正负号的判断，只需要一次。
-          故，可以降低其判断的优先级
-          */
-          this.sign = char === '+' ? 1 : -1;
-        }
-      }
     }
-  
+
     // 生成自动机实例
     let automaton = new Automaton();
-  
+
     // 遍历每个字符
-    for(let char of str) {
-      // 依次进行转换
-      automaton.get(char);
+    for (let char of str) {
+        // 依次进行转换
+        automaton.get(char);
     }
-  
+
     // 返回值，整数 = 正负 * 数值
     return automaton.sign * automaton.answer;
-  };
+};
 /* 19.给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 */
 // 思路：快慢指针发，先让快指针走n步，然后快慢指针一起走，等快指针到链表末尾，慢指针的下一节点便是要删除节点
-var removeNthFromEnd = function(head, n) {
+var removeNthFromEnd = function (head, n) {
     let fast = head, slow = head;
-    for(let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         fast = fast.next;
     }
-    if(!fast){
+    if (!fast) {
         return head.next;
     }
-    while(fast.next){
+    while (fast.next) {
         fast = fast.next;
         slow = slow.next;
     }
     slow.next = slow.next.next;
     return head;
+};
+/* 23.合并k个升序链表
+给你一个链表数组，每个链表都已经按升序排列。
+请你将所有链表合并到一个升序链表中，返回合并后的链表。
+思路：转换成数组，排序再转为链表
+  */
+var mergeKLists = function (lists) {
+    let arr = [];
+    for (let i = 0; i < lists.length; i++) {
+        let list = lists[i];
+        while (list) {
+            arr.push(list.val);
+            list = list.next
+        }
+    }
+    arr.sort((a, b) => a - b)
+    const head = new ListNode();
+    let now = head;
+    for (let i = 0; i < arr.length; i++) {
+        now.next = new ListNode(arr[i]);
+        now = now.next;
+    }
+    return head.next;
+};
+/* 思路2.两两合并 */
+var mergeKLists = function (lists) {
+    if (!lists.length) return null
+    //递归出口，即数组中只剩一条链表时，合并完毕
+    if (lists.length === 1) return lists[0]
+
+    //两个一组的合并，合并完了更新数组（每次合并前两个）
+    lists.splice(0, 2, mergeTwoLists(lists[0], lists[1]))
+    //递归
+    return mergeKLists(lists)
+};
+
+//尾插法合并两个链表
+function mergeTwoLists(l1, l2) {
+    let head = new ListNode(), pre = head
+    while (l1 && l2) {
+        if (l1.val > l2.val) {
+            pre.next = l2
+            l2 = l2.next
+        } else {
+            pre.next = l1
+            l1 = l1.next
+        }
+        pre = pre.next
+    }
+    pre.next = l1 ? l1 : l2
+    return head.next
+};
+/* 31.实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+
+如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/next-permutation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+/* 思路：怎么让数变大，还是尽量大一点：找到左比右小的第一个位置，让最靠右的比该数大的数字和该书互换位置，剩下从该数的下一位置开始再互换一下位置让数值更小一些 */
+var nextPermutation = function (nums) {
+    let i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+    if (i >= 0) {
+        let j = nums.length - 1;    
+        while (j > i && nums[j] <= nums[i]) {
+            j--
+        }
+        [nums[j], nums[i]] = [nums[i], nums[j]];
+    }
+    i++;
+    let r = nums.length - 1;
+    while (r > i) {
+        if (nums[i] > nums[r]) {
+            [nums[r], nums[i]] = [nums[i], nums[r]];
+        }
+        i++;
+        r--;
+    }
 };
