8165d0022d899d544ac385d736ae6c95a34bdaaf
Jack0110 <wangjianping@sjtu.edu.cn>
2021-11-18 10:01:16 +0800

    2021.11.17

--- dataStructure/LeetcodeHot100.js
+++ dataStructure/LeetcodeHot100.js
@@ -637,3 +637,24 @@ var canJump = function(nums) {
     }
     return false;
 };
+
+/* 56.合并区间
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+
+ intervals[i].length == 2*/
+var merge = function (intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    let prev = intervals[0]
+    let result = []
+    for (let i = 0; i < intervals.length; i++) {
+        let cur = intervals[i]
+        if (cur[0] > prev[1]) {
+            result.push(prev)
+            prev = cur
+        } else {
+            prev[1] = Math.max(cur[1], prev[1])
+        }
+    }
+    result.push(prev)
+    return result
+}
--- dataStructure/clone.js
+++ dataStructure/clone.js
@@ -21,3 +21,62 @@ function deepClone(obj){
     }
     return newObj;
 }
+function clone(obj) {
+    // 当null NaN undefined number string等基本数据类型时直接返回
+    if (obj === null || typeof obj !== 'object') {
+      return obj
+    }
+    // Date类型
+    if (obj instanceof Date) {
+      const copy = new Date()
+      copy.setTime(obj.getTime())
+      return copy
+    }
+    // 正则类型类型
+    if (obj instanceof RegExp) {
+      const Constructor = obj.constructor
+      return new Constructor(obj)
+    }
+    // 如果是数组等引用数据类型
+    if (obj instanceof Array || obj instanceof Object) {
+      const copyObj = Array.isArray(obj) ? [] : {}
+      for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          copyObj[key] = clone(obj[key])
+        }
+      }
+      return copyObj
+    }
+  }
+  function deepClone(target){
+    if(target instanceof Object){
+        let dist ;
+        if(target instanceof Array){
+          // 拷贝数组
+          dist = [];
+        }else if(target instanceof Function){
+          // 拷贝函数
+          dist = function () {
+            return target.call(this, ...arguments);
+          };
+        }else if(target instanceof RegExp){
+          // 拷贝正则表达式
+         dist = new RegExp(target.source,target.flags);
+        }else if(target instanceof Date){
+            dist = new Date(target);
+        }else{
+          // 拷贝普通对象
+          dist = {};
+        }
+        for(let key in target){
+            // 过滤掉原型身上的属性
+          if (target.hasOwnProperty(key)) {
+              dist[key] = deepClone(target[key]);
+          }
+          // dist[key] = deepClone(target[key]);
+        }
+        return dist;
+    }else{
+        return target;
+    }
+  }
