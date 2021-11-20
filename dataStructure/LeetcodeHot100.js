d0b2ef3e6696b08109837274bfd3fd9d737e369b
Jack0110 <wangjianping@sjtu.edu.cn>
2021-11-20 18:43:05 +0800

    2021.11.20pm

--- dataStructure/LeetcodeHot100.js
+++ dataStructure/LeetcodeHot100.js
@@ -897,3 +897,26 @@ var mergeTrees = function (root1, root2) {
     }
     return preOrder(root1, root2);
 };
+
+/* 75.颜色分类 
+给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-colors
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+// 思路：关注0和2，一遍遍历，每个i处
+var sortColors = function (nums) {
+    let p0 = 0, p1 = nums.length - 1;
+    for (let i = 0; i <= p1; i++) {
+        while (i < p1 && nums[i] == 2) {
+            [nums[i], nums[p1]] = [nums[p1], nums[i]];
+            p1--;
+        }
+        if (nums[i] == 0) {
+            [nums[i], nums[p0]] = [nums[p0], nums[i]];
+            p0++;
+        }
+    }
+};
