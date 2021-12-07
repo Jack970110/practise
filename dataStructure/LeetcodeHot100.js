825596d2f699db5f133ee315d78095be496c9779
Jack0110 <wangjianping@sjtu.edu.cn>
2021-12-07 09:39:59 +0800

    2021.12.06

--- dataStructure/LeetcodeHot100.js
+++ dataStructure/LeetcodeHot100.js
@@ -1494,3 +1494,77 @@ var restoreIpAddresses = function(s) {
     slice(s,'',4)
     return res
 };
+
+/* 139.单词划分 
+给你一个字符串 s 和一个字符串列表 wordDict 作为字典，判定 s 是否可以由空格拆分为一个或多个在字典中出现的单词。
+
+说明：拆分时可以重复使用字典中的单词。*/
+// 动态规划
+var wordBreak = function(s, wordDict) {
+    const wordSet = new Set(wordDict);
+    const len = s.length;
+    const dp = new Array(len + 1).fill(false);
+    dp[0] = true;
+
+    for (let i = 1; i <= len; i++) {
+    for (let j = i - 1; j >= 0; j--) {    // j去划分成两部分
+        if (dp[i] == true) break;
+        if (dp[j] == false) continue;
+        const suffix = s.slice(j, i);       // 后缀部分 s[j: i-1]
+        if (wordSet.has(suffix) && dp[j]) { // 后缀部分是单词，且左侧子串[0,j-1]的dp[j]为真
+            dp[i] = true;
+            break;  // dp[i] = true了，i长度的子串已经可以拆成单词了，不需要j继续划分子串了
+        }
+    }
+    }
+    return dp[len];
+};
+// dfs
+const wordBreak = (s, wordDict) => {
+    const len = s.length;
+    const wordSet = new Set(wordDict);
+    const memo = new Array(len);
+  
+    const canBreak = (start) => {
+      if (start == len) return true;
+      if (memo[start] !== undefined) return memo[start]; // memo中有，就用memo中的
+  
+      for (let i = start + 1; i <= len; i++) {
+        const prefix = s.slice(start, i);
+        if (wordSet.has(prefix) && canBreak(i)) {
+          memo[start] = true; // 当前递归的结果存一下
+          return true;
+        }
+      }
+      memo[start] = false; // 当前递归的结果存一下
+      return false;
+    };
+    return canBreak(0);
+  };
+// bfs
+const wordBreak = (s, wordDict) => {
+    const wordSet = new Set(wordDict);
+    const len = s.length;
+    const visited = new Array(len);
+  
+    const queue = [];
+    queue.push(0);
+  
+    while (queue.length) {
+      const start = queue.shift();  // 考察出列的指针
+      if (visited[start]) continue; // 是访问过的，跳过
+      visited[start] = true;        // 未访问过的，记录一下
+  
+      for (let i = start + 1; i <= len; i++) { // 用指针i去划分两部分
+        const prefix = s.slice(start, i);      // 前缀部分
+        if (wordSet.has(prefix)) {  // 前缀部分是单词
+          if (i < len) {            // i还没越界，还能继续划分，让它入列，作为下一层待考察的节点
+            queue.push(i);
+          } else {  // i==len，指针越界，说明s串一路被切出单词，现在没有剩余子串，不用划分，返回true
+            return true;
+          }
+        } // 前缀部分不是单词，i指针不入列，继续下轮迭代，切出下一个前缀部分，再试
+      }
+    }
+    return false; // BFS完所有节点（考察了所有划分的可能）都没返回true，则返回false
+  };
