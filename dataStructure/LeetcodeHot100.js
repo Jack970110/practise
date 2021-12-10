819950866e1cbdfc18fea5c2bd9e58e115897a7d
Jack0110 <wangjianping@sjtu.edu.cn>
2021-12-10 11:08:11 +0800

    2021.12.10

--- dataStructure/LeetcodeHot100.js
+++ dataStructure/LeetcodeHot100.js
@@ -1616,3 +1616,150 @@ var detectCycle = function(head) {
     }
     return null;
 };
+
+/* 148.排序链表 
+给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+
+进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？*/
+// 暴力
+var sortList = function(head) {
+    if(!head) {
+        return null;
+    }
+    let arr = [];
+    while(head) {
+        arr.push(head.val);
+        head = head.next;
+    }
+    arr.sort((a,b) => a - b);
+    let pre = new ListNode(0,null), Chead = pre;
+    for(let i = 0; i < arr.length; i++) {
+        let cur = new ListNode(arr[i],null);
+        Chead.next = cur;
+        Chead = Chead.next;
+    }
+    return pre.next;
+};
+var sortList = function(head) {
+    if(!head) { // 边界判断
+        return null;
+    }
+    let arr = []; // 数组存储打断的链表节点
+    while(head) { // 打断节点
+        let t = head.next;
+        head.next = null;
+        arr.push(head);
+        head = t;
+    }
+    arr.sort((a, b) => a.val - b.val);// 排序
+    for(let i = 0; i < arr.length - 1; i++) {
+        arr[i].next = arr[i+1]; // 链接
+    }
+    return arr[0];
+}
+// 归并 自顶向下
+const merge = (head1, head2) => {
+    const dummyHead = new ListNode(0);
+    let temp = dummyHead, temp1 = head1, temp2 = head2;
+    while (temp1 !== null && temp2 !== null) {//合并子区间 小的节点先连
+        if (temp1.val <= temp2.val) {
+            temp.next = temp1;
+            temp1 = temp1.next;
+        } else {
+            temp.next = temp2;
+            temp2 = temp2.next;
+        }
+        temp = temp.next;
+    }
+    if (temp1 !== null) {//两条链表还有节点没合并完，直接合并过来
+        temp.next = temp1;
+    } else if (temp2 !== null) {
+        temp.next = temp2;
+    }
+    return dummyHead.next;
+}
+
+const toSortList = (head, tail) => {
+    if (head === null) {//极端情况
+        return head;
+    }
+    if (head.next === tail) {//分割到只剩一个节点
+        head.next = null;
+        return head;
+    }
+    let slow = head, fast = head;
+    while (fast !== tail) {//的到中间节点
+        slow = slow.next;
+        fast = fast.next;
+        if (fast !== tail) {
+            fast = fast.next;
+        }
+    }
+    const mid = slow;
+    return merge(toSortList(head, mid), toSortList(mid, tail));//分割区间 递归合并
+}
+
+var sortList = function(head) {
+    return toSortList(head, null);
+};
+// 自底向上
+const merge = (head1, head2) => {
+    const dummyHead = new ListNode(0);
+    let temp = dummyHead, temp1 = head1, temp2 = head2;
+    while (temp1 !== null && temp2 !== null) {
+        if (temp1.val <= temp2.val) {
+            temp.next = temp1;
+            temp1 = temp1.next;
+        } else {
+            temp.next = temp2;
+            temp2 = temp2.next;
+        }
+        temp = temp.next;
+    }
+    if (temp1 !== null) {
+        temp.next = temp1;
+    } else if (temp2 !== null) {
+        temp.next = temp2;
+    }
+    return dummyHead.next;
+}
+
+var sortList = function(head) {
+    if (head === null) {
+        return head;
+    }
+    let length = 0;
+    let node = head;
+    while (node !== null) {
+        length++;
+        node = node.next;
+    }
+    const dummyHead = new ListNode(0, head);
+    for (let subLength = 1; subLength < length; subLength <<= 1) {
+        let prev = dummyHead, curr = dummyHead.next;
+        while (curr !== null) {
+            let head1 = curr;
+            for (let i = 1; i < subLength && curr.next !== null; i++) {
+                curr = curr.next;
+            }
+            let head2 = curr.next;
+            curr.next = null;
+            curr = head2;
+            for (let i = 1; i < subLength && curr != null && curr.next !== null; i++) {
+                curr = curr.next;
+            }
+            let next = null;
+            if (curr !== null) {
+                next = curr.next;
+                curr.next = null;
+            }
+            const merged = merge(head1, head2);
+            prev.next = merged;
+            while (prev.next !== null) {
+                prev = prev.next;
+            }
+            curr = next;
+        }
+    }
+    return dummyHead.next;
+};
