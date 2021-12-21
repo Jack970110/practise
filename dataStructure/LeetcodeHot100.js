ff7ab594647b6831e4facc50a405780b9c73f51c
Jack970110 <wangjianping@sjtu.edu.cn>
2021-12-21 10:58:01 +0800

    2021.12.21

--- dataStructure/LeetcodeHot100.js
+++ dataStructure/LeetcodeHot100.js
@@ -1985,3 +1985,73 @@ Trie.prototype.search = function(word) {
 Trie.prototype.startsWith = function(prefix) {
     return this.searchPrefix(prefix);
 };
+
+/* 221.最大正方形
+在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。 */
+/* 暴力求解
+找到一个值为1的元素，把它当做是正方形的左上角元素，每次新增一行一列并且判断新增的所有元素都为1，都为1当前正方形边长+1；记录最大边长，乘积即为最大面积 
+时间复杂度：O(mn *min(m,n)2)
+空间复杂度：O（1）*/
+var maximalSquare = function(matrix) {
+    let maxSide = 0;
+    if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+        return maxSide;
+    }
+    let m = matrix.length, n = matrix[0].length;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] == '1') {
+                // 遇到一个 1 作为正方形的左上角
+                maxSide = Math.max(maxSide, 1);
+                // 计算可能的最大正方形边长
+                let currentMaxSide = Math.min(m - i, n - j);
+                for (let k = 1; k < currentMaxSide; k++) {
+                    // 判断新增的一行一列是否均为 1
+                    let flag = true;
+                    if (matrix[i + k][j + k] == '0') {
+                        break;
+                    }
+                    for (let temp = 0; temp < k; temp++) {
+                        if (matrix[i + k][j + temp] == '0' || matrix[i + temp][j + k] == '0') {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag) {
+                        maxSide = Math.max(maxSide, k + 1);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    let maxSquare = maxSide * maxSide;
+    return maxSquare;
+};
+/* 动态规划
+ dp[i][j]表示以（i，j）为右下角的正方形边长最大值，只有当前元素matrix[i][j]为1的时候，最大正方形才可能转移，转移方程为：
+    dp(i,j)=min(dp(i−1,j),dp(i−1,j−1),dp(i,j−1))+1
+ 边界条件：当i或j有一个为0，且当前元素值为1，dp[i][j]为1
+ 时间空间复杂度：O（mn）*/
+var maximalSquare = function(matrix) {
+    if(!matrix.length) {
+        return 0;
+    }
+    let m = matrix.length, n = matrix[0].length, max = Number.MIN_VALUE;
+    let dp = new Array(m).fill().map(() =>{return new Array(n).fill(0)});
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j <n; j++) {
+            if(matrix[i][j] == "1"){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1;
+                }
+                max = Math.max(dp[i][j], max);
+            }
+            
+        }
+    } 
+    return max * max;
+};
