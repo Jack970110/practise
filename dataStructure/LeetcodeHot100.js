97c7ce0ee9ea93ec9b28261a1303ea62784722f6
Jack0110 <wangjianping@sjtu.edu.cn>
2022-03-08 17:34:41 +0800

    fixed 2022.03.08

--- dataStructure/LeetcodeHot100.js
+++ dataStructure/LeetcodeHot100.js
@@ -470,23 +470,25 @@ var trap = function (height) {
     return ans;
 }
 // 单调栈
-let ans = 0;
-const stack = [];
-const n = height.length;
-for (let i = 0; i < n; ++i) {
-    while (stack.length && height[i] > height[stack[stack.length - 1]]) {
-        const top = stack.pop();
-        if (!stack.length) {
-            break;
+var trap = function (height) {
+    let ans = 0;
+    const stack = [];
+    const n = height.length;
+    for (let i = 0; i < n; ++i) {
+        while (stack.length && height[i] > height[stack[stack.length - 1]]) {
+            const top = stack.pop();
+            if (!stack.length) {
+                break;
+            }
+            const left = stack[stack.length - 1];
+            const currWidth = i - left - 1;
+            const currHeight = Math.min(height[left], height[i]) - height[top];
+            ans += currWidth * currHeight;
         }
-        const left = stack[stack.length - 1];
-        const currWidth = i - left - 1;
-        const currHeight = Math.min(height[left], height[i]) - height[top];
-        ans += currWidth * currHeight;
+        stack.push(i);
     }
-    stack.push(i);
+    return ans;
 }
-return ans;
 
 /*46.全排列
 回溯经典问题*/
